5ef6e17cf46a5ea4ec076dbe900f9840dad904ec
ayejay-nz <55215693+ayejay-nz@users.noreply.github.com>
2026-02-03 10:22:04 +1300

    Add functions to create initial bitboards for white/black

---
+++ main.py
@@ -0,0 +1,38 @@
+def rank_to_row(raw_row, rank):
+    """
+    Convert a singular row into a row given its rank 
+
+    Example: raw_row = 255 (11111111), rank = 2 -> 65280 (1111111100000000)
+    """
+
+    return raw_row << (8 * (rank - 1))
+
+def init_bitboards(white_it_bottom=True): 
+    """
+    Initialise bitboards for white and black, returning a tuple of the white and black bitboard array, in that order.
+
+    bb1 = pawns, bb2 = rooks, bb3 = knight, bb4 = bishop, bb5 = queen, bb6 = king
+
+    Bottom left of board is index 0, incrementing from left to right and up
+    """
+
+    pawns_row = 0b11111111
+    rooks_row = 0b10000001
+    knights_row = 0b01000010
+    bishops_row = 0b00100100
+    d_file_bit = 0b00010000
+    e_file_bit = 0b00001000
+
+    # Create bitboards for bottom player excluding king/queen
+    bitboards_bottom = [rank_to_row(pawns_row, 2), rooks_row, knights_row, bishops_row]
+    # Create bitboards for top player excluding king/queen
+    bitboards_top = [rank_to_row(pawns_row, 7), rank_to_row(rooks_row, 8), rank_to_row(knights_row, 8), rank_to_row(bishops_row, 8)]
+
+    if white_it_bottom:
+        bitboards_bottom.extend([d_file_bit, e_file_bit])
+        bitboards_top.extend([rank_to_row(d_file_bit, 8), rank_to_row(e_file_bit, 8)])
+        return (bitboards_bottom, bitboards_top)
+    else: 
+        bitboards_bottom.extend([e_file_bit, d_file_bit])
+        bitboards_top.extend([rank_to_row(e_file_bit, 8), rank_to_row(d_file_bit, 8)])
+        return (bitboards_top, bitboards_bottom)
