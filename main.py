3df4d5c7fd86a3ee8540170d9f6318de0b9eccf6
ayejay-nz <55215693+ayejay-nz@users.noreply.github.com>
2026-02-10 03:17:15 +1300

    Fix bug with `output_boardstate` function
     - Black and white piece variables were inverted
     - Output the players pieces at the bottom of the board correctly

--- main.py
+++ main.py
@@ -62,8 +62,8 @@ def output_boardstate(white_bbs, black_bbs, is_white):
     Output the current boardstate from the white/black bitboards
     """
 
-    white_piece_order = ['♙', '♖', '♘', '♗', '♕', '♔']
-    black_piece_order = ['♟', '♜', '♞', '♝', '♛', '♚']
+    black_piece_order = ['♙', '♖', '♘', '♗', '♕', '♔']
+    white_piece_order = ['♟', '♜', '♞', '♝', '♛', '♚']
     board = ['.'] * 64
 
     for idx, bb in enumerate(white_bbs):
@@ -83,11 +83,11 @@ def output_boardstate(white_bbs, black_bbs, is_white):
             bb ^= lsb
 
     lines = []
-    for rank in range(8):
+    for rank in range(7, -1, -1):
         row = board[rank * 8: (rank + 1) * 8]
         
-        if is_white: lines.append(str(8 - rank) + ' ' + ' '.join(row))
-        else: lines.append(str(rank + 1) + ' ' + ' '.join(row))
+        if is_white: lines.append(str(rank + 1) + ' ' + ' '.join(row))
+        else: lines.append(str(8 - rank) + ' ' + ' '.join(row))
         
     if is_white: lines.append('  a b c d e f g h')
     else: lines.append('  h g f e d c b a')
