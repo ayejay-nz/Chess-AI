3b4a1c9480e7a2f3eb3df58fe5e0e1b1f7793a70
ayejay-nz <55215693+ayejay-nz@users.noreply.github.com>
2026-02-10 03:43:00 +1300

    Initialise all bitboards to have a1 as 0 index

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def rank_to_row(raw_row, rank):
 
     return raw_row << (8 * (rank - 1))
 
-def init_bitboards(white_is_bottom=True): 
+def init_bitboards(): 
     """
     Initialise bitboards for white and black, returning a tuple of the white and black bitboard array, in that order.
 
     bb1 = pawns, bb2 = rooks, bb3 = knight, bb4 = bishop, bb5 = queen, bb6 = king
 
-    Bottom left of board is index 0, incrementing from left to right and up
+    Bottom left of board (a1) is index 0, incrementing from left to right and down to up, until reaching top right (h8)
     """
 
     pawns_row = 0b11111111
@@ -28,19 +28,12 @@ def init_bitboards(white_is_bottom=True):
     d_file_bit = 0b00001000
     e_file_bit = 0b00010000
 
-    # Create bitboards for bottom player excluding king/queen
-    bitboards_bottom = [rank_to_row(pawns_row, 2), rooks_row, knights_row, bishops_row]
-    # Create bitboards for top player excluding king/queen
-    bitboards_top = [rank_to_row(pawns_row, 7), rank_to_row(rooks_row, 8), rank_to_row(knights_row, 8), rank_to_row(bishops_row, 8)]
+    # Create bitboards for white
+    bitboards_white = [rank_to_row(pawns_row, 2), rooks_row, knights_row, bishops_row, d_file_bit, e_file_bit]
+    # Create bitboards for black
+    bitboards_black = [rank_to_row(pawns_row, 7), rank_to_row(rooks_row, 8), rank_to_row(knights_row, 8), rank_to_row(bishops_row, 8), rank_to_row(d_file_bit, 8), rank_to_row(e_file_bit, 8)]
 
-    if white_is_bottom:
-        bitboards_bottom.extend([d_file_bit, e_file_bit])
-        bitboards_top.extend([rank_to_row(d_file_bit, 8), rank_to_row(e_file_bit, 8)])
-        return (bitboards_bottom, bitboards_top)
-    else: 
-        bitboards_bottom.extend([e_file_bit, d_file_bit])
-        bitboards_top.extend([rank_to_row(e_file_bit, 8), rank_to_row(d_file_bit, 8)])
-        return (bitboards_top, bitboards_bottom)
+    return (bitboards_white, bitboards_black)
 
 def user_wants_white():
     """
@@ -90,7 +83,7 @@ def get_move(is_white):
 
 def main():
     is_white = user_wants_white()
-    white_bbs, black_bbs = init_bitboards(is_white)
+    white_bbs, black_bbs = init_bitboards()
     output_boardstate(white_bbs, black_bbs, is_white)
 
     capturing_moves, pawn_moves = find_legal_moves(white_bbs, black_bbs, is_white, is_whites_move, castling_rights)
